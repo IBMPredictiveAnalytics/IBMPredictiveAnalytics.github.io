585c6ca91a8af538789f69c8f2651e92de419cae
aruizga7 <armand.ruiz@gmail.com>
2015-04-15 14:31:48 -0500

    First commit

---
+++ orgs.js
@@ -0,0 +1,9 @@
+
+    var orgs = [
+    	{"name": "IBMPredictiveAnalytics",
+    	 "type": "org",
+    	 "link": "http://ibmpredictiveanalytics.github.io/",
+		 "name": "aruizga7",
+    	 "type": "user",
+    	 "link": "http://ibmpredictiveanalytics.github.io/"}
+    ];
