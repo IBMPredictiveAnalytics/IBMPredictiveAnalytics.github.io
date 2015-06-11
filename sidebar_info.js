fd7ae4c2424ba9beb7230ce90980a82d72b454ce
wujiazhong <wujz@ADMINIB-31C38UD.spss.com>
2015-06-11 09:39:17 +0800

    update

---
+++ sidebar_info.js
@@ -0,0 +1,35 @@
+/*icon name code are derived from font-awesome,
+  to the icon-code table, please visit http://fortawesome.github.io/Font-Awesome/icons/
+*/
+var sidebar_info = [
+	{
+		"menu_name":"Software",
+		"menu_icon_name": "fa-dashboard",
+		"sub_menu_name":["SPSS Modeler","SPSS Statistics"],
+		"sub_menu_icon_name":["fa-diamond","fa-table"]
+	},
+	{
+		"menu_name":"Type",
+		"menu_icon_name": "fa-cubes",
+		"sub_menu_name":["Extension","Native Application","Module"],
+		"sub_menu_icon_name":["fa-plus-square-o","fa-folder","fa-cube"]
+	},	
+	{
+		"menu_name":"Category",
+		"menu_icon_name": "fa-dashboard",
+		"sub_menu_name":["Analysis","Data Manipulation","Utility","Geospatial","Visualization","Watson"],
+		"sub_menu_icon_name":["fa-line-chart","fa-database","fa-wrench","fa-globe","fa-bar-chart","fa-cloud"]
+	},
+	{
+		"menu_name":"Language",
+		"menu_icon_name": "fa-code",
+		"sub_menu_name":["Java","Native product","Python 2","Python 3","R"],
+		"sub_menu_icon_name":["fa-code","fa-code","fa-code","fa-code","fa-code"]
+	},
+	{
+		"menu_name":"Provider",
+		"menu_icon_name": "fa-user",
+		"sub_menu_name":["IBM","Business Partner","Community"],
+		"sub_menu_icon_name":["fa-star","fa-circle","fa-user-plus"]
+	}	
+];
