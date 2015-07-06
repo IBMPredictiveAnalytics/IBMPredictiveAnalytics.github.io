bedea46d221faba5e986f6d2037ec3cc94dd1ca1
wujiazhong <wujz@ADMINIB-31C38UD.spss.com>
2015-07-06 09:49:49 +0800

    solve problem of delay of resizing

--- js/jquery.ba-resize.js
+++ js/jquery.ba-resize.js
@@ -70,7 +70,7 @@
   // The numeric interval (in milliseconds) at which the resize event polling
   // loop executes. Defaults to 250.
   
-  jq_resize[ str_delay ] = 250;
+  jq_resize[ str_delay ] = 25;
   
   // Property: jQuery.resize.throttleWindow
   // 
